9b650382c73d3655e7569bf45f5af63dff586741
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-22 12:08:22 -0300

    Exercicio de fixação de linq com lambda.

---
+++ ExFixLinq/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using ExFixLinq.Entities;
+
+namespace ExFixLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Cursos\CSharp\ExFixLinq\ExFixLinq\Files\input.txt";
+
+            double limit = 2000;
+
+            List<Employee> list = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] data = sr.ReadLine().Split(',');
+                    string name = data[0];
+                    string email = data[1];
+                    double salary = double.Parse(data[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, email, salary));
+                }
+            }
+
+            var people = list.Where(p => p.Salary > limit).OrderBy(p => p.Email).Select(p => p.Email);
+            
+            var sum = list.Where(p => p.Name[0] == 'M').Sum(s => s.Salary);
+
+            foreach(string p in people){
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine($"Soma de salários iniciados com M: {sum.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
